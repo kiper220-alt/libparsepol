9486676c5d85b9e90573a25cd7f907a6e22dc280
kiper220-alt <gedertkj@basealt.ru>
2024-07-29 17:02:21 +0400

    wip: test case

--- test/testcases.hpp
+++ test/testcases.hpp
@@ -17,20 +17,28 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <cstring>
+#include <fstream>
 #include <iostream>
+
 #include <parser.hpp>
 
-#include "./binary.hpp"
-#include "./endian.hpp"
-#include "./testcases.hpp"
-
-#include <iconv.h>
-
-int main()
+void testCase1()
 {
-    testEndian();
-    testBinary();
-    testCase1();
+    std::ifstream file("../rsc/case1.pol", std::ios::in | std::ios::binary);
 
-    return 0;
+    if (!file.is_open()) {
+        std::cerr << "Can't open file `rsc/case1.pol`!" << std::endl;
+        return;
+    }
+
+    auto parser = createPregParser();
+
+    std::optional<PolicyFile> data = parser->parse(file);
+
+    if (!data.has_value() || !data->body.has_value()) {
+        std::cerr << "Can't parse file `rsc/case1.pol`!" << std::endl;
+        return;
+    }
+    assert(std::get<std::string>(data->body->instructions[0].data) == std::string("'r[e]d'"));
 }
